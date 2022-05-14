067aea1d710cdb2b883b78cb1e36a260197bb9d0
mw <martinez2@gmx.de>
2022-05-14 15:23:00 +0200

    feat: Add method show for ProductStore

--- src/models/product.ts
+++ src/models/product.ts
@@ -45,13 +45,34 @@ export class ProductStore {
 	 */
 	async index(): Promise<Product[]> {
 		try {
-			const sql = 'SELECT * FROM products';
+			const sql = 'SELECT * FROM products;';
 			const conn = await db.connect();
 			const result = await conn.query(sql);
 			conn.release();
 			return result.rows;
 		} catch (err) {
 			throw new Error(`Unable to get list of products.\n${err}`);
+		}
+	}
+
+	/**
+	 * @description Get product for a given id.
+	 * @param id - The id of the product
+	 * @returns - The product with this id
+	 */
+	async show(id: string): Promise<Product> {
+		try {
+			const sql = 'SELECT * FROM products WHERE id = $1;';
+			const conn = await db.connect();
+			const result = await conn.query(sql, [id]);
+			conn.release();
+			if (result.rows.length > 0) {
+				return result.rows[0];
+			} else {
+				throw new Error();
+			}
+		} catch (err) {
+			throw new Error(`Unable to get product for id ${id}\n${err}`);
 		}
 	}
 }
