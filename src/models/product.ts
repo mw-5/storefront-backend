87ea8604e46778cc666ee7e334697e4a0d3ccd7f
mw <martinez2@gmx.de>
2022-05-14 12:43:12 +0200

    feat: Add ProductStore with create method

---
+++ src/models/product.ts
@@ -0,0 +1,41 @@
+import db from '../database';
+
+/**
+ * @description Data of table products
+ */
+export type Product = {
+	// Use string for BIGINT
+	id?: string;
+	name: string;
+	price: string;
+	category_id: string;
+};
+
+/**
+ * @description Provides CRUD methods for table products
+ * of the database.
+ */
+export class ProductStore {
+	/**
+	 * @description Create provided product in the database.
+	 * @param p - The product that should be created
+	 * @returns - The new product
+	 */
+	async create(p: Product): Promise<Product> {
+		try {
+			const sql =
+				'INSERT INTO products (name, price, category_id)' +
+				' VALUES ($1, $2, $3) RETURNING *;';
+			const conn = await db.connect();
+			const result = await conn.query(sql, [
+				p.name,
+				p.price,
+				p.category_id,
+			]);
+			conn.release();
+			return result.rows[0];
+		} catch (err) {
+			throw new Error(`Unable to create product ${p}.\n${err}`);
+		}
+	}
+}
