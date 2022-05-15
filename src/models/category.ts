0d5ef109b1d6676940fd270fa199faf8eb48dedd
mw <martinez2@gmx.de>
2022-05-15 09:50:21 +0200

    feat: Add type Category and class CategoryStore

---
+++ src/models/category.ts
@@ -0,0 +1,17 @@
+import db from '../database';
+
+/**
+ * @description Data of table categories
+ */
+export type Category = {
+    id?: string;
+    name: string;
+}
+
+/**
+ * @description Provides CRUD methods for table
+ * categories of the database.
+ */
+export class CategoryStore {
+
+}
