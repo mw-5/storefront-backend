d76e82df7261b20a35bc83a50964b3d0642272f1
mw <martinez2@gmx.de>
2022-05-16 22:11:32 +0200

    test: Add tests for method create of UserStore

---
+++ spec/tests/models/userSpec.ts
@@ -0,0 +1,125 @@
+import db from '../../../src/database';
+import { User, UserStore } from '../../../src/models/user';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+describe('Testsuite UserStore:', () => {
+	const store = new UserStore();
+	// Use this id for prepopulated user
+	const TEST_ID = 'testUsername';
+	// Use this id for user created during test
+	const TEST_ID_NEW = 'testNewUsername';
+	const TEST_FIRST_NAME = 'testFirstName';
+	const TEST_LAST_NAME = 'testLastname';
+	const TEST_UNHASHED_PW = 'test123';
+	const SALT_ROUNDS = process.env.SALT_ROUNDS as string;
+	const PEPPER = process.env.BCRYPT_PASSWORD as string;
+
+	/**
+	 * @description Populate users table.
+	 */
+	const populateTestDb = async (): Promise<void> => {
+		const conn = await db.connect();
+
+		const sql =
+			'INSERT INTO users (id, first_name, last_name, password_digest)' +
+			' VALUES ($1, $2, $3, $4);';
+		const hashedPassword = bcrypt.hashSync(
+			TEST_UNHASHED_PW + PEPPER,
+			parseInt(SALT_ROUNDS)
+		);
+		await conn.query(sql, [
+			TEST_ID,
+			TEST_FIRST_NAME,
+			TEST_LAST_NAME,
+			hashedPassword,
+		]);
+
+		conn.release();
+	};
+
+	/**
+	 * @description Empty users table.
+	 */
+	const emptyTestDb = async (): Promise<void> => {
+		const conn = await db.connect();
+
+		const sql = 'DELETE FROM users;';
+		await conn.query(sql);
+
+		conn.release();
+	};
+
+	/**
+	 * @description Create a user for use in tests.
+	 * @returns
+	 */
+	const createTestUser = (): User => {
+		return {
+			id: TEST_ID,
+			first_name: TEST_FIRST_NAME,
+			last_name: TEST_LAST_NAME,
+			password: TEST_UNHASHED_PW,
+			password_digest: '',
+		};
+	};
+
+	describe('Test expects method create', () => {
+		beforeEach(populateTestDb);
+		afterEach(emptyTestDb);
+
+		it('to be defined', () => {
+			expect(store.create).toBeDefined();
+		});
+
+		it('to create correct password for new user', async () => {
+			// Arrange
+			const expectedUser = createTestUser();
+			// Assign this id to avoid conflict with
+			// prepopulated user
+			expectedUser.id = TEST_ID_NEW;
+
+			// Act
+			const resultUser = await store.create(expectedUser);
+
+			// Assert
+			expect(
+				bcrypt.compareSync(
+					expectedUser.password + PEPPER,
+					resultUser.password_digest
+				)
+			).toBeTrue();
+		});
+
+		it('to create correct user', async () => {
+			// Arrange
+			const expectedUser = createTestUser();
+			// Assign this id to avoid conflict with
+			// prepopulated user
+			expectedUser.id = TEST_ID_NEW;
+
+			// Act
+			const resultUser = await store.create(expectedUser);
+
+			// Assert
+			// For test on password creation see separate test
+			expectedUser.password = '0';
+			expectedUser.password_digest = '0';
+			resultUser.password = '0';
+			resultUser.password_digest = '0';
+			expect(resultUser).toEqual(expectedUser);
+		});
+
+		it('to throw error if user already exists', async () => {
+			// Arrange
+			const expectedUser = createTestUser();
+
+			// Act & Assert
+			await expectAsync(
+				store.create(expectedUser)
+			).toBeRejectedWithError();
+		});
+	});
+});
