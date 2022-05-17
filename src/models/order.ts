662a17fca8f1c6da496efd25115ab99f2830bcd1
mw <martinez2@gmx.de>
2022-05-17 11:34:03 +0200

    feat: Add method create to OrderStore

--- src/models/order.ts
+++ src/models/order.ts
@@ -13,4 +13,25 @@ export type Order = {
  * @description Provides methods for interacting
  * with table orders of the database.
  */
-export class OrderStore {}
+export class OrderStore {
+	/**
+	 * @description Create a new order in the database.
+	 * @param userId - The id of the user associated with the order
+	 * @returns - The created order
+	 */
+	async create(userId: string): Promise<Order> {
+		try {
+			const sql =
+				'INSERT INTO orders (user_id, status)' +
+				' VALUES ($1, $2) RETURNING *;';
+			const conn = await db.connect();
+			const result = await conn.query(sql, [userId, 'active']);
+			conn.release();
+			return result.rows[0];
+		} catch (err) {
+			throw new Error(
+				`Unable to create order for user ${userId}.\n${err}`
+			);
+		}
+	}
+}
