707245b27d6eddc964dc0e0fec0d9ec1e7070d65
mw <martinez2@gmx.de>
2022-05-21 21:14:51 +0200

    test: Refactor productsSpec to use testutils

--- spec/tests/handlers/productsSpec.ts
+++ spec/tests/handlers/productsSpec.ts
@@ -1,79 +1,19 @@
 import supertest from 'supertest';
 import app from '../../../src/server';
-import { Product } from '../../../src/models/product';
-import db from '../../../src/database';
+import * as tu from '../../tests/testutils';
 
 const request = supertest(app);
 
 describe('Testsuite for products routes', () => {
 	const ROUTE = '/products';
-	const TEST_PRODUCT_NAME = 'testProduct';
-	// PostgreSQL BIGINT is represented as string
-	const TEST_PRODUCT_PRICE = '100';
-	const TEST_CATEGORY_ID = '1';
-	const TEST_CATEGORY_NAME = 'testCategory';
-
-	/**
-	 * @description Populate tables products and categories.
-	 */
-	const populateTestDb = async (): Promise<void> => {
-		let sql: string;
-		const conn = await db.connect();
-
-		// Populate table categories
-		sql = 'INSERT INTO categories (id, name) VALUES ($1, $2);';
-		await conn.query(sql, [TEST_CATEGORY_ID, TEST_CATEGORY_NAME]);
-
-		// Populate table products
-		sql =
-			'INSERT INTO products (name, price, category_id)' +
-			' VALUES ($1, $2, $3);';
-		await conn.query(sql, [
-			TEST_PRODUCT_NAME,
-			TEST_PRODUCT_PRICE,
-			TEST_CATEGORY_ID,
-		]);
-
-		conn.release();
-	};
-
-	/**
-	 * @description Empty tables products and categories.
-	 */
-	const emptyTestDb = async (): Promise<void> => {
-		let sql: string;
-		const conn = await db.connect();
-
-		// Empty table products
-		sql = 'DELETE FROM products;';
-		await conn.query(sql);
-
-		// Empty table categories
-		sql = 'DELETE FROM categories;';
-		await conn.query(sql);
-
-		conn.release();
-	};
-
-	/**
-	 * @description Create a product to be used in tests.
-	 * @returns - The test product
-	 */
-	const createTestProduct = (): Product => {
-		return {
-			name: TEST_PRODUCT_NAME,
-			price: TEST_PRODUCT_PRICE,
-			category_id: TEST_CATEGORY_ID,
-		};
-	};
 
 	describe('Test for endpoint POST products', () => {
-		beforeEach(populateTestDb);
-		afterEach(emptyTestDb);
+		beforeEach(tu.populateTestDb);
+		afterEach(tu.emptyTestDb);
 
 		it('expects status code 200 on success', async () => {
 			// Arrange
-			const newProduct = createTestProduct();
+			const newProduct = tu.createTestProduct();
 
 			// Act
 			const response = await request.post(ROUTE).send(newProduct);
@@ -84,7 +24,7 @@ describe('Testsuite for products routes', () => {
 
 		it('expects response to contain new product', async () => {
 			// Arrange
-			const expectedProduct = createTestProduct();
+			const expectedProduct = tu.createTestProduct();
 
 			// Act
 			const response = await request.post(ROUTE).send(expectedProduct);
@@ -100,7 +40,7 @@ describe('Testsuite for products routes', () => {
 
 		it('expects status code 400 on invalid input', async () => {
 			// Arrange
-			const newProduct = createTestProduct();
+			const newProduct = tu.createTestProduct();
 			newProduct.category_id = '-1';
 
 			// Act
