d76dc71fd61efc65ccda0ea7364b2993a21583e9
mw <martinez2@gmx.de>
2022-05-21 14:59:22 +0200

    feat: Add handler and endpoint to create product

---
+++ src/handlers/products.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from 'express';
+import { Product, ProductStore } from '../models/product';
+
+const store = new ProductStore();
+
+// Handlers
+
+/**
+ * @description Handle create request for product.
+ * @param req - The incoming request
+ * @param res - The response send
+ * @returns - The created product
+ */
+const create = async (req: Request, res: Response): Promise<void> => {
+	try {
+		const product: Product = {
+			name: req.body.name,
+			price: req.body.price,
+			category_id: req.body.category_id,
+		};
+		const newProduct = await store.create(product);
+		res.json(newProduct);
+	} catch (err) {
+		res.status(400);
+		res.json(err);
+	}
+};
+
+// Endpoints
+
+/**
+ * @description Set up endpoints to handle requests for products.
+ * @param app - The express application
+ */
+const productRoutes = (app: express.Application): void => {
+	app.post('/products', create);
+};
+
+export default productRoutes;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import productsRoutes from './handlers/products';
 
 const app: express.Application = express();
 const address = '0.0.0.0:3000';
@@ -12,6 +13,10 @@ app.get('/', function (req: Request, res: Response) {
 	res.send('Hello World!');
 });
 
+productsRoutes(app);
+
 app.listen(3000, function () {
 	console.log(`starting app on: ${address}`);
 });
+
+export default app;
