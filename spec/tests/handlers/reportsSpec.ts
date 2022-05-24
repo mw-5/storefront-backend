d36df2579379309ab621e113f75dddaeb2aa46c0
mw <martinez2@gmx.de>
2022-05-24 14:27:51 +0200

    test: Add tests for GET users/:id/completed_orders

--- spec/tests/handlers/reportsSpec.ts
+++ spec/tests/handlers/reportsSpec.ts
@@ -1,6 +1,7 @@
 import supertest from 'supertest';
 import app from '../../../src/server';
 import * as tu from '../../tests/testutils';
+import { Order, OrderStore } from '../../../src/models/order';
 
 const request = supertest(app);
 
@@ -48,6 +49,52 @@ describe('Testsuite for reporting routes', () => {
 			// Act
 			const response = await request.get(
 				'/users/notExistingUser/current_order'
+			);
+
+			// Assert
+			expect(response.statusCode).toEqual(400);
+		});
+	});
+
+	describe('Test for endpoint GET users/:id/completed_orders', () => {
+		beforeEach(tu.populateTestDb);
+		afterEach(tu.emptyTestDb);
+
+		it('expects status code 200 on success', async () => {
+			// Arrange
+			const store = new OrderStore();
+			await store.complete(tu.ORDER_ID);
+
+			// Act
+			const response = await request.get(
+				`/users/${tu.USER_ID}/completed_orders`
+			);
+
+			// Assert
+			expect(response.statusCode).toEqual(200);
+		});
+
+		it('expects to receive completed orders', async () => {
+			// Arrange
+			const store = new OrderStore();
+			await store.complete(tu.ORDER_ID);
+
+			// Act
+			const response = await request.get(
+				`/users/${tu.USER_ID}/completed_orders`
+			);
+			const resultOrders = response.body;
+
+			// Assert
+			resultOrders.forEach((resultOrder: Order) => {
+				expect(resultOrder.is_completed).toBeTrue();
+			});
+		});
+
+		it('expects status code 400 for missing completed orders', async () => {
+			// Act
+			const response = await request.get(
+				`/users/notExistingUser/completed_orders`
 			);
 
 			// Assert
