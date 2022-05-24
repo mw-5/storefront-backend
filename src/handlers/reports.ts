b322f13f189771b7e7af72128f32b1249c37bf33
mw <martinez2@gmx.de>
2022-05-24 14:29:38 +0200

    feat: Add EP & handler for report completed orders

--- src/handlers/reports.ts
+++ src/handlers/reports.ts
@@ -24,6 +24,25 @@ const currentOrderByUser = async (
 	}
 };
 
+/**
+ * @description Handle request for completed orders by user.
+ * @param req - The incoming request
+ * @param res - The response send
+ */
+const completedOrdersByUser = async (
+	req: Request,
+	res: Response
+): Promise<void> => {
+	try {
+		const userId = req.params.id;
+		const orders = await queries.completedOrdersByUser(userId);
+		res.json(orders);
+	} catch (err) {
+		res.status(400);
+		res.json(err);
+	}
+};
+
 // Endpoints
 
 /**
@@ -32,6 +51,7 @@ const currentOrderByUser = async (
  */
 const reportRoutes = (app: express.Application): void => {
 	app.get('/users/:id/current_order', currentOrderByUser);
+	app.get('/users/:id/completed_orders', completedOrdersByUser);
 };
 
 export default reportRoutes;
