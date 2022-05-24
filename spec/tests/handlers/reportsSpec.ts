50a22dafa113d11e9392facabae13ab203391c29
mw <martinez2@gmx.de>
2022-05-24 14:57:15 +0200

    test: Add tests for endpoint GET top 5 products

--- spec/tests/handlers/reportsSpec.ts
+++ spec/tests/handlers/reportsSpec.ts
@@ -2,6 +2,7 @@ import supertest from 'supertest';
 import app from '../../../src/server';
 import * as tu from '../../tests/testutils';
 import { Order, OrderStore } from '../../../src/models/order';
+import { ProductStore } from '../../../src/models/product';
 
 const request = supertest(app);
 
@@ -135,6 +136,52 @@ describe('Testsuite for reporting routes', () => {
 
 			// Assert
 			expect(response.statusCode).toEqual(400);
+		});
+	});
+
+	describe('Test for endpoint GET top_5_popular_products', () => {
+		beforeEach(tu.populateTestDb);
+		afterEach(tu.emptyTestDb);
+		const ROUTE = '/top_5_popular_products';
+
+		it('expects status code 200 on success', async () => {
+			// Act
+			const response = await request.get(ROUTE);
+
+			// Assert
+			expect(response.statusCode).toEqual(200);
+		});
+
+		it('expects to return an array of five', async () => {
+			// Arrange
+			const p0 = {
+				name: tu.PRODUCT_NAME,
+				category_id: tu.CATEGORY_ID,
+				price: tu.PRODUCT_PRICE,
+			};
+			const productStore = new ProductStore();
+			const orderStore = new OrderStore();
+			// Create products and add them to the cart
+			const p1 = await productStore.create(p0);
+			await orderStore.addProduct(tu.ORDER_ID, <string>p1.id, 10);
+			const p2 = await productStore.create(p0);
+			await orderStore.addProduct(tu.ORDER_ID, <string>p2.id, 15);
+			await orderStore.addProduct(tu.ORDER_ID, <string>p2.id, 15);
+			const p3 = await productStore.create(p0);
+			await orderStore.addProduct(tu.ORDER_ID, <string>p3.id, 11);
+			const p4 = await productStore.create(p0);
+			await orderStore.addProduct(tu.ORDER_ID, <string>p4.id, 12);
+			const p5 = await productStore.create(p0);
+			await orderStore.addProduct(tu.ORDER_ID, <string>p5.id, 20);
+			const p6 = await productStore.create(p0);
+			await orderStore.addProduct(tu.ORDER_ID, <string>p6.id, 5);
+
+			// Act
+			const response = await request.get(ROUTE);
+			const result = response.body;
+
+			// Assert
+			expect(result.length).toEqual(5);
 		});
 	});
 });
