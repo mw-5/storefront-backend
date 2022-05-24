a9c0ce2004b136a722801d2f18280f57f8c4715d
mw <martinez2@gmx.de>
2022-05-24 11:26:15 +0200

    test: Add tests for endpoint POST users/login

--- spec/tests/handlers/usersSpec.ts
+++ spec/tests/handlers/usersSpec.ts
@@ -2,7 +2,7 @@ import supertest from 'supertest';
 import app from '../../../src/server';
 import * as tu from '../../tests/testutils';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { User } from '../../../src/models/user';
 
@@ -87,6 +87,55 @@ describe('Testsuite for users routes', () => {
 
 			// Assert
 			expect(response.statusCode).toEqual(400);
+		});
+	});
+
+	describe('Test for endpoint POST users/login', () => {
+		beforeEach(tu.populateTestDb);
+		afterEach(tu.emptyTestDb);
+
+		it('expects status code 200 on sucess', async () => {
+			// Arrange
+			const user = tu.createTestUser();
+
+			// Act
+			const response = await request.post(`${ROUTE}/login`).send(user);
+
+			// Assert
+			expect(response.statusCode).toEqual(200);
+		});
+
+		it('expects to receive JWT with correct user id', async () => {
+			// Arrange
+			const expectedUser = tu.createTestUser();
+
+			// Act
+			const response = await request
+				.post(`${ROUTE}/login`)
+				.send(expectedUser);
+			// Extract user from response
+			const token = response.body;
+			const payload = <JwtPayload>(
+				jwt.verify(token, <string>process.env.SECRET_TOKEN)
+			);
+			const resultUser = <User>payload.user;
+
+			// Assert
+			expect(resultUser.id).toEqual(expectedUser.id);
+		});
+
+		it('expects status code 401 for unauthorized user', async () => {
+			// Arrange
+			const user = {
+				id: 'notExistingUser',
+				password: '123',
+			};
+
+			// Act
+			const response = await request.post(`${ROUTE}/login`).send(user);
+
+			// Assert
+			expect(response.statusCode).toEqual(401);
 		});
 	});
 });
