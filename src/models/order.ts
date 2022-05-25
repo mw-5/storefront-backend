79e2772509908b83b6896f8b38f31bb008d0a8ba
mw <martinez2@gmx.de>
2022-05-25 08:45:57 +0200

    feat: Add method show to OrderStore

--- src/models/order.ts
+++ src/models/order.ts
@@ -117,4 +117,48 @@ export class OrderStore {
 			);
 		}
 	}
+
+	/**
+	 * @description Get order for given id.
+	 * @param orderId - The id of the order
+	 * @returns - The order
+	 */
+	async show(orderId: string): Promise<Order> {
+		try {
+			// Build query
+			const sql =
+				'SELECT op.order_id, o.user_id, op.product_id,' +
+				' op.quantity, o.is_completed' +
+				' FROM (orders AS o INNER JOIN order_products AS op' +
+				' ON o.id = op.order_id)' +
+				' INNER JOIN products AS p ON op.product_id = p.id' +
+				' WHERE o.id = $1;';
+
+			// Execute query
+			const conn = await db.connect();
+			const result = await conn.query(sql, [orderId]);
+			conn.release();
+
+			// Check if query returned results
+			if (result.rowCount === 0) {
+				throw new Error();
+			}
+
+			// Build order
+			const products: { id: string; quantity: number }[] = [];
+			result.rows.forEach((row) => {
+				products.push({ id: row.product_id, quantity: row.quantity });
+			});
+			const order: Order = {
+				id: result.rows[0].order_id,
+				products: products,
+				user_id: result.rows[0].user_id,
+				is_completed: result.rows[0].is_completed,
+			};
+
+			return order;
+		} catch (err) {
+			throw new Error(`Unable to get order ${orderId}.\n${err}`);
+		}
+	}
 }
