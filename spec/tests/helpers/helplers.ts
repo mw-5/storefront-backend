3e9fcd018d8d5f915828791e5673af1aa2197751
mw <martinez2@gmx.de>
2022-05-30 14:54:52 +0200

    test: Add custom reporter

---
+++ spec/tests/helpers/helplers.ts
@@ -0,0 +1,26 @@
+/**
+ * Configuration according to documentation at:
+ * https://github.com/bcaudan/jasmine-spec-reporter/tree/master/examples/typescript
+ */
+ import {
+	DisplayProcessor,
+	SpecReporter,
+	StacktraceOption,
+} from 'jasmine-spec-reporter';
+import SuiteInfo = jasmine.SuiteInfo;
+
+class CustomProcessor extends DisplayProcessor {
+	public displayJasmineStarted(info: SuiteInfo, log: string): string {
+		return `TypeScript ${log}`;
+	}
+}
+
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(
+	new SpecReporter({
+		spec: {
+			displayStacktrace: StacktraceOption.NONE,
+		},
+		customProcessors: [CustomProcessor],
+	})
+);
